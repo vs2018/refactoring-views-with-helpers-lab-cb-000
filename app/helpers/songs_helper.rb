f210b486db9aa8ea55fc5cb0cd8335d83b85e2e9
Vishal Sharma <vsh1819@gmail.com>
2018-07-23 11:10:24 +0000

    automatically backed up by learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,12 +1,12 @@
 module SongsHelper
   
-  # def display_artist(song)
-  #   if song.artist
-  #     link_to song.artist.name, artist_path(song.artist) 
-  #   else
-  #     link_to "Add Artist", edit_song_path(song)
-  #   end
-  # end
+  def display_artist(song)
+    if song.artist
+      link_to song.artist.name, artist_path(song.artist) 
+    else
+      link_to "Add Artist", edit_song_path(song)
+    end
+  end
 
 end
 
